69a1b9154575f7f50450e5ab4c1a80796af8501f
Mariana-Sanchez21 <marysansam21@gmail.com>
2023-03-22 09:05:07 -0500

    updates

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,131 @@
-const cipher = {
-  // ...
-};
 
-export default cipher;
+function shiftLetter (letter,shift){
+    let newLetter="";
+
+   let letterCode=letter.charCodeAt(0);
+    let newLetterCode= (letterCode+ shift-65) % 26 +65 ;
+
+     if (newLetterCode<97){
+       newLetter+=26;
+    }else if(newLetterCode>122){
+      newLetterCode-=26;
+    }
+    newLetter=String.fromCharCode(newLetterCode);
+
+return newLetter;
+}
+
+function shiftLetter2 (letter2,shift2){
+  let newLetter2="";
+
+ let letterCode2=letter2.charCodeAt(0);
+  let newLetterCode2= (letterCode2- shift2+65) % 26 +65 ;
+
+   if (newLetterCode2<97){
+     newLetter2+=26;
+  }else if(newLetterCode2>122){
+    newLetterCode2-=26;
+  }
+  newLetter2=String.fromCharCode(newLetterCode2);
+
+return newLetter2;
+}
+
+ const cipher={
+   encode:function(message,shift){
+
+    
+        let encryptedMessage= "";    
+      let shift2= shift ? Number (shift):0;
+      
+          for(const letter of message){
+            encryptedMessage += shiftLetter(letter,shift2);
+           }
+      
+          return encryptedMessage;
+          
+           
+      
+        },
+        decode:function(message2,shift2){
+          let encryptedMessage2="";
+          let shift3=  shift2 ? Number (shift2):0;
+
+          for(let letter2 of message2){
+            encryptedMessage2+=shiftLetter2(letter2,shift3);
+          }
+
+          return encryptedMessage2;
+        }
+}
+
+
+  export default cipher;  
+  
+
+  
+  
+
+  
+
+  
+
+   
+  
+
+
+
+
+
+
+
+
+
+
+// //     function inicio() {
+
+// //       document.getElementById("mensajeacifrar1").addEventListener("keyup",function(){
+// //             this.value= this.value.toLowerCase();
+
+// //       },true);
+
+// //       document.getElementById("mensajeadescifrar1").addEventListener("Keyup",function(){
+// //         this.value= this.value.toLowerCase();
+
+// //       },true);
+
+// //       document.getElementById("cifrar2").addEventListener("click",function(){
+// //         let mensajeOriginal =document.getElementById("mensajeacifrar1").value;
+// //         let posiciones=document.getElementById("numeroacifrar1").value;
+// //         document.getElementById("cifrado").value= cifrar(mensajeOriginal,posiciones)
+// //       },true);
+
+// //         document.getElementById("descifrar2").addEventListener("click",function(){
+// //       let mensajeOriginal2 =document.getElementById("mensajeadescifrar1").value;
+// //       let posiciones2=document.getElementById("numeroadescifrar1").value;
+// //       document.getElementById("descifrado").value= descifrar(mensajeOriginal2,posiciones2)
+// //         },true);
+// //     }
+
+// //    function cifrar(mensajeOriginal,posiciones){
+// //     let resultadoCifrado= "";
+// //     let letras ="abcdefghijklmnopqrstuvwxyz";
+
+// //     posiciones=(posiciones % 26+26) %26 ;
+
+// //     posiciones2=(posiciones2 % 26+26) %26 ;
+
+// // if(mensajeOriginal){
+// //   for(let i=0; i<mensajeOriginal.length; i++){
+// //       if (letras.indexOf(mensajeOriginal(i)) !=-1){
+// //         let posicion=((letras.indexOf(mensajeOriginal[i])+posiciones)%26);
+// //         resultadoCifrado+= letras[posicion];
+
+// //       } else
+// //           resultadoCifrado+= mensajeOriginal[i];
+
+// //   }
+// //    }
+// // return resultadoCifrado
+// //    }
+   
--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+ 
+
+    const originalInput = document.getElementById("mensajeacifrar1");
+  const shiftInput = document.getElementById("numeroacifrar1");
+   const encryptedInput = document.getElementById("cifrado");
+ const botonCif= document.getElementById("cifrar2");
+ botonCif.addEventListener("click",function(){
+  const inputValue=originalInput.value;
+  let shiftValue=shiftInput.value;
+  const result=cipher.encode(inputValue,shiftValue);
+   encryptedInput.value=result;
+     console.log("Prueba")
+   });
+
+
+  const originalInput2 = document.getElementById("mensajeadescifrar1");
+  const shiftInput2 = document.getElementById("numeroadescifrar1");
+  const encryptedInput2 = document.getElementById("descifrado");
+  const BotonDescif= document.getElementById("descifrar2");
+  BotonDescif.addEventListener("click",function(){
+
+        let inputValue2=originalInput2.value;
+        let shiftValue2=shiftInput2.value;
+        const result2=cipher.decode(inputValue2,shiftValue2);
+        encryptedInput2.value=result2;
+        console.log("Prueba2")
+
+      });
+
+    
+
+
